d89e7e2ac4809975bef583ee4bad5c06f1184f30
Ravish Kumar <ravish.kumar@chromium.org>
2025-05-22 00:00:30 +0530

    Revert "Integration"
    This reverts commit 382bddc722501a2227edb209d40cc7edb554e4a3.

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://task-manager-1-es9c.onrender.com'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: 'http://localhost:5173',
+  credentials: true,             
 }));
 
 
